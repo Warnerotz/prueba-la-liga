538d1701555b9f6e72b0635bedfcb2aed524741a
Adrian Martinez <adrian.martinezalvarez@soprasteria.com>
2022-03-04 13:33:51 +0100

    add api calls and redux data.

--- src/store/userDetail/userDetail.reducer.js
+++ src/store/userDetail/userDetail.reducer.js
@@ -24,10 +24,48 @@ const getUserDetailFailure = (state, { payload }) => ({
   error: payload,
 });
 
+const deleteUserStart = (state) => ({
+  ...state,
+  isLoading: true,
+});
+
+const deleteUserSuccess = (state) => ({
+  ...state,
+  isLoading: false,
+});
+
+const deleteUserFailure = (state, { payload }) => ({
+  ...state,
+  isLoading: false,
+  error: payload,
+});
+
+const updateUserStart = (state) => ({
+  ...state,
+  isLoading: true,
+});
+
+const updateUserSuccess = (state) => ({
+  ...state,
+  isLoading: false,
+});
+
+const updateUserFailure = (state, { payload }) => ({
+  ...state,
+  isLoading: false,
+  error: payload,
+});
+
 const reducerMap = {
   [UserDetailTypes.GET_USER_DETAIL_START]: getUserDetailStart,
   [UserDetailTypes.GET_USER_DETAIL_SUCCESS]: getUserDetailSuccess,
   [UserDetailTypes.GET_USER_DETAIL_FAILURE]: getUserDetailFailure,
+  [UserDetailTypes.DELETE_USER_START]: deleteUserStart,
+  [UserDetailTypes.DELETE_USER_SUCCESS]: deleteUserSuccess,
+  [UserDetailTypes.DELETE_USER_FAILURE]: deleteUserFailure,
+  [UserDetailTypes.UPDATE_USER_START]: updateUserStart,
+  [UserDetailTypes.UPDATE_USER_SUCCESS]: updateUserSuccess,
+  [UserDetailTypes.UPDATE_USER_FAILURE]: updateUserFailure,
 };
 
 const reducer = (state = INITIAL_STATE, action) =>
