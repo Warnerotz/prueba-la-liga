86816b3c4422cd8069eb9a3f20a60aad32301131
Adrian Martinez <adrian.martinezalvarez@soprasteria.com>
2022-03-08 10:31:18 +0100

    solve some test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,44 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import configureStore from "redux-mock-store";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
+const initialState = {
+  users: {
+    isLoading: false,
+    error: null,
+    usersList: {
+      data: [
+        {
+          id: 1,
+          first_name: "adrian",
+          last_name: "martinez",
+          email: "adrian@adrian.com",
+        },
+      ],
+    },
+  },
+  userDetail: {
+    userDetail: null,
+    isLoading: false,
+    error: null,
+  },
+  authToken: {
+    authToken: "QpwL5tke4Pnpja7X1",
+    isLoading: false,
+    error: null,
+  },
+};
+
+const mockStore = configureStore([thunk]);
 test("renders learn react link", () => {
-  render(<App />);
+  render(
+    <Provider store={mockStore(initialState)}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
 });
